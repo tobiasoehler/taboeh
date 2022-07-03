a2db8e83dc953284a42af858508db091035fb8a7
Tobias Oehler <tobiasoehler@live.de>
2022-07-03 18:47:19 +0200

    init spring boot project

---
+++ src/main/java/de/emsato/taboeh/TaboehApplication.java
@@ -0,0 +1,13 @@
+package de.emsato.taboeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaboehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaboehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/emsato/taboeh/TaboehApplicationTests.java
@@ -0,0 +1,13 @@
+package de.emsato.taboeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaboehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
