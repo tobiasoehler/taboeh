6b85d948a19ec48a13d28ed9fa46e23e962d0fbd
DESKTOP-J0O7OV8\Tobias Oehler <tobiasoehler@live.de>
2022-07-20 23:07:07 +0200

    add custom login page

---
+++ src/main/java/de/emsato/taboeh/configuration/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package de.emsato.taboeh.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login.html")
+                .failureUrl("/login-error.html")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/index.html");
+        return http.build();
+    }
+
+}
